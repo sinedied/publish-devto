f2a6a819d7be3873c6a09912b6ab6edd18a29136
sinedied <noda@free.fr>
2021-10-05 21:42:43 +0200

    feat: migrate to devto-cli

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const core = require('@actions/core');
-const { publishArticles } = require('./publish');
+import core from '@actions/core';
+import { publishArticles } from './lib/publish.js';
 
 async function run() {
   try {
-    const filesGlob = core.getInput('files');
     const devtoKey = core.getInput('devto_key');
     const githubToken = core.getInput('github_token');
+    const filesGlob = core.getInput('files');
+    const branch = core.getInput('branch');
     const useConventionalCommits = core.getInput('conventional_commits');
+
     core.setSecret(devtoKey);
     core.setSecret(githubToken);
+
     core.debug(
       JSON.stringify({
-        filesGlob,
         devtoKey,
         githubToken,
+        filesGlob,
+        branch,
         useConventionalCommits
       })
     );
@@ -22,6 +26,7 @@ async function run() {
       filesGlob,
       devtoKey,
       githubToken,
+      branch,
       useConventionalCommits
     });
   } catch (error) {
