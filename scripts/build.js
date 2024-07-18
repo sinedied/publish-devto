6ca0bb08e68b016887b58f73964f8c6948957be7
sinedied <noda@free.fr>
2024-07-18 11:41:38 +0200

    chore: update packages

--- scripts/build.js
+++ scripts/build.js
@@ -8,11 +8,11 @@ fs.mkdirSync('dist');
 ncc(path.join(process.cwd(), 'index.js'), {
   minify: true
 }).then(({ code }) => {
-  const pkg = JSON.stringify({ type: 'module' }, null, 2);
-  fs.writeFileSync('dist/package.json', pkg);
+  const package_ = JSON.stringify({ type: 'module' }, null, 2);
+  fs.writeFileSync('dist/package.json', package_);
 
   // Fix issue with node: protocol imports
-  code = code.replace(/"node:/g, '"');
+  code = code.replaceAll('"node:', '"');
   fs.writeFileSync('dist/index.js', code);
 
   console.log('Done.');
